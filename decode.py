b0ced16e65d8725995b33fdcd0a0e35c1b9ed816
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-11 21:08:10 +0100

    Add files via upload

--- decode.py
+++ decode.py
@@ -1,12 +1,12 @@
 print("\033c\033[47;31m\ngive me a utf-16 .bin utf-16 file to encript ? ")
 a=input().strip()
 b=a.replace(".bin","")
-f1=open(a,"r",encoding="utf-16-le")
+f1=open(a,"r",encoding="utf-16")
 f=f1.read()
 f1.close()
-r=f.encode("utf-8")
+
 counter=0
 
-f1=open(b+".txt","wb")
-f1.write(r)
+f1=open(b+".txt","w",encoding="utf-8")
+f1.write(f)
 f1.close()
